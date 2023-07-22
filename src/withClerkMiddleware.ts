fbec852fd92abd927c56936b2ea7a812a5501d68
wobsoriano <sorianorobertc@gmail.com>
2023-07-21 18:51:47 -0700

    feat:  Introduce createIsomorphicRequest

--- src/withClerkMiddleware.ts
+++ src/withClerkMiddleware.ts
@@ -4,27 +4,20 @@
  * Credits to the Clerk team.
  * https://github.com/clerkinc/javascript/blob/main/packages/fastify/src/withClerkMiddleware.ts
  */
-import type { ClerkOptions, SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend'
-import { eventHandler, getHeaders, parseCookies, setResponseHeaders, setResponseStatus } from 'h3'
+import { type ClerkOptions, type SignedInAuthObject, type SignedOutAuthObject, createIsomorphicRequest } from '@clerk/backend'
+import { eventHandler, getHeaders, getMethod, setResponseHeaders, setResponseStatus } from 'h3'
 import * as constants from './constants'
 import { getSingleValueFromArrayHeader } from './utils'
 import { clerkClient } from './clerkClient'
 
 export type ClerkH3Options = Omit<ClerkOptions, 'apiKey'>
 
+const DUMMY_URL_BASE = 'http://clerk-dummy'
+
 export function withClerkMiddleware(options: ClerkH3Options) {
   return eventHandler(async (event) => {
-    const cookies = parseCookies(event)
     const secretKey = options.secretKey || constants.SECRET_KEY
     const publishableKey = options.publishableKey || constants.PUBLISHABLE_KEY
-    const headers = getHeaders(event)
-
-    // get auth state, check if we need to return an interstitial
-    const cookieToken = cookies[constants.Cookies.Session]
-    const headerToken = headers.authorization?.replace('Bearer ', '')
-
-    const forwardedPort = getSingleValueFromArrayHeader(headers['x-forwarded-port'])
-    const forwardedHost = getSingleValueFromArrayHeader(headers['x-forwarded-host'])
 
     const requestState = await clerkClient.authenticateRequest({
       ...options,
@@ -32,15 +25,30 @@ export function withClerkMiddleware(options: ClerkH3Options) {
       publishableKey,
       apiKey: constants.API_KEY,
       frontendApi: constants.FRONTEND_API,
-      cookieToken,
-      headerToken,
-      clientUat: cookies[constants.Cookies.ClientUat],
-      origin: headers.origin,
-      host: headers.host,
-      forwardedPort,
-      forwardedHost,
-      referrer: headers.referer,
-      userAgent: headers['user-agent'],
+      request: createIsomorphicRequest((Request, Headers) => {
+        const requestHeaders = Object.keys(getHeaders(event)).reduce(
+          (acc, key) => Object.assign(acc, { [key]: getHeaders(event)[key] }),
+          {},
+        )
+        const headers = new Headers(requestHeaders)
+        const forwardedHostHeader = getSingleValueFromArrayHeader(
+          headers?.get(constants.Headers.ForwardedHost) || undefined,
+        )
+        if (forwardedHostHeader)
+          headers.set(constants.Headers.ForwardedHost, forwardedHostHeader)
+
+        const forwardedPortHeader = getSingleValueFromArrayHeader(
+          headers?.get(constants.Headers.ForwardedPort) || undefined,
+        )
+        if (forwardedPortHeader)
+          headers.set(constants.Headers.ForwardedPort, forwardedPortHeader)
+
+        const reqUrl = isRelativeUrl(event.node.req.url!) ? getAbsoluteUrlFromHeaders(event.node.req.url!, headers) : event.node.req.url!
+        return new Request(reqUrl, {
+          method: getMethod(event),
+          headers,
+        })
+      }),
     })
 
     // Interstitial cases
@@ -69,6 +77,51 @@ export function withClerkMiddleware(options: ClerkH3Options) {
 
     event.context.auth = requestState.toAuth()
   })
+}
+
+// TODO: Move the utils below to shared package
+// Creating a Request object requires a valid absolute URL
+// Fastify's req.url is relative, so we need to construct an absolute URL
+function getAbsoluteUrlFromHeaders(url: string, headers: Headers): URL {
+  const forwardedProto = headers.get(constants.Headers.ForwardedProto)
+  const forwardedPort = headers.get(constants.Headers.ForwardedPort)
+  const forwardedHost = headers.get(constants.Headers.ForwardedHost)
+
+  const fwdProto = getFirstValueFromHeaderValue(forwardedProto)
+  let fwdPort = getFirstValueFromHeaderValue(forwardedPort)
+
+  // If forwardedPort mismatch with forwardedProto determine forwardedPort
+  // from forwardedProto as fallback (if exists)
+  // This check fixes the Railway App issue
+  const fwdProtoHasMoreValuesThanFwdPorts
+    = (forwardedProto || '').split(',').length > (forwardedPort || '').split(',').length
+  if (fwdProto && fwdProtoHasMoreValuesThanFwdPorts)
+    fwdPort = getPortFromProtocol(fwdProto)
+
+  try {
+    return new URL(url, `${fwdProto}://${forwardedHost}${fwdPort ? `:${fwdPort}` : ''}`)
+  }
+  catch (e) {
+    return new URL(url, DUMMY_URL_BASE)
+  }
+}
+
+const PROTOCOL_TO_PORT_MAPPING: Record<string, string> = {
+  http: '80',
+  https: '443',
+} as const
+
+function getPortFromProtocol(protocol: string) {
+  return PROTOCOL_TO_PORT_MAPPING[protocol]
+}
+
+function getFirstValueFromHeaderValue(value?: string | null) {
+  return value?.split(',')[0]?.trim() || ''
+}
+
+function isRelativeUrl(url: string) {
+  const u = new URL(url, DUMMY_URL_BASE)
+  return u.origin === DUMMY_URL_BASE
 }
 
 declare module 'h3' {
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,8 @@
+import createFetchMock from 'vitest-fetch-mock'
+import { vi } from 'vitest'
+
+const fetchMocker = createFetchMock(vi)
+fetchMocker.enableMocks()
+
 process.env.CLERK_API_KEY = 'TEST_API_KEY'
 process.env.CLERK_SECRET_KEY = 'TEST_API_KEY'
