daa601d8137ca7d3274cfdd2e599b2c4a8ec798e
wobsoriano <sorianorobertc@gmail.com>
2024-03-03 20:45:45 -0800

    add temporary fix warning

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ function fixProtoHeaderInDevMode(event: H3Event) {
 type H3ClerkMiddlewareOptions = ClerkMiddlewareOptions & {
   /**
    * Adjusts the `x-forwarded-proto` header in development mode to match the protocol of the request. Temporary hacky fix to https://github.com/nuxt/nuxt/issues/23348.
+   * Defaults to `false` in the next major release.
    */
   adjustProtoHeaderInDev?: boolean
 }
