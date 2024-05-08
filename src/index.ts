356f67bf3362af720cdaec50b808c183fee28ae9
wobsoriano <sorianorobertc@gmail.com>
2024-05-07 21:37:51 -0700

    fix: hotfix for express status missing function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { ClerkExpressWithAuth } from '@clerk/clerk-sdk-node'
-import { eventHandler, fromNodeMiddleware } from 'h3'
+import { eventHandler, fromNodeMiddleware, setResponseStatus } from 'h3'
 import type { EventHandler } from 'h3'
 import type { ClerkMiddlewareOptions } from '@clerk/clerk-sdk-node'
 import type { SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend/dist/internal'
 
 export function withClerkMiddleware(options?: ClerkMiddlewareOptions) {
   return eventHandler({
-    onRequest: [fromNodeMiddleware(ClerkExpressWithAuth(options))],
+    onRequest: [
+      eventHandler((event) => {
+        // @ts-expect-error: Patch res.status() from express
+        event.node.res.status = (statusCode: number) => {
+          event.node.res.statusCode = statusCode
+          return event.node.res
+        }
+      }),
+      fromNodeMiddleware(ClerkExpressWithAuth(options)),
+    ],
     async handler(event) {
       // @ts-expect-error: Clerk Node attaches auth object to req.auth
       event.context.auth = event.node.req.auth
@@ -16,7 +25,16 @@ export function withClerkMiddleware(options?: ClerkMiddlewareOptions) {
 
 export function withClerkAuth(handler: EventHandler, options?: ClerkMiddlewareOptions) {
   return eventHandler({
-    onRequest: [fromNodeMiddleware(ClerkExpressWithAuth(options))],
+    onRequest: [
+      eventHandler((event) => {
+        // @ts-expect-error: Patch res.status() from express
+        event.node.res.status = (statusCode: number) => {
+          event.node.res.statusCode = statusCode
+          return event.node.res
+        }
+      }),
+      fromNodeMiddleware(ClerkExpressWithAuth(options)),
+    ],
     async handler(event) {
       // @ts-expect-error: Clerk Node attaches auth object to req.auth
       event.context.auth = event.node.req.auth
