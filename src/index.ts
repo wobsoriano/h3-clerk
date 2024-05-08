055f3ed4b3044c228a6bb40800798671ee8ee486
wobsoriano <sorianorobertc@gmail.com>
2024-05-07 20:54:31 -0700

    chore: remove unused types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { ClerkExpressWithAuth } from '@clerk/clerk-sdk-node'
-import { eventHandler, fromNodeMiddleware, getRequestProtocol } from 'h3'
-import type { EventHandler, H3Event, NodeMiddleware } from 'h3'
-import type { ClerkMiddlewareOptions, SignedInAuthObject, SignedOutAuthObject } from '@clerk/clerk-sdk-node'
+import { eventHandler, fromNodeMiddleware } from 'h3'
+import type { EventHandler } from 'h3'
+import type { ClerkMiddlewareOptions } from '@clerk/clerk-sdk-node'
+import type { SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend/dist/internal'
 
 export function withClerkMiddleware(options?: ClerkMiddlewareOptions) {
   return eventHandler({
-    onRequest: [fromNodeMiddleware(ClerkExpressWithAuth(options) as NodeMiddleware)],
+    onRequest: [fromNodeMiddleware(ClerkExpressWithAuth(options))],
     async handler(event) {
       // @ts-expect-error: Clerk Node attaches auth object to req.auth
       event.context.auth = event.node.req.auth
@@ -15,7 +16,7 @@ export function withClerkMiddleware(options?: ClerkMiddlewareOptions) {
 
 export function withClerkAuth(handler: EventHandler, options?: ClerkMiddlewareOptions) {
   return eventHandler({
-    onRequest: [fromNodeMiddleware(ClerkExpressWithAuth(options) as NodeMiddleware)],
+    onRequest: [fromNodeMiddleware(ClerkExpressWithAuth(options))],
     async handler(event) {
       // @ts-expect-error: Clerk Node attaches auth object to req.auth
       event.context.auth = event.node.req.auth
@@ -25,7 +26,7 @@ export function withClerkAuth(handler: EventHandler, options?: ClerkMiddlewareOp
   })
 }
 
-export { Clerk, clerkClient, createClerkClient } from '@clerk/clerk-sdk-node'
+export * from '@clerk/clerk-sdk-node'
 
 declare module 'h3' {
   interface H3EventContext {
