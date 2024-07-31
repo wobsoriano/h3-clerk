98e253f21d88ea74659c8a591ebe3150c4420905
wobsoriano <sorianorobertc@gmail.com>
2024-07-31 10:25:32 -0700

    ci: fix release script

--- src/constants.ts
+++ src/constants.ts
@@ -5,10 +5,5 @@ export const SECRET_KEY = process.env.CLERK_SECRET_KEY || ''
 export const PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY || ''
 export const API_URL = process.env.CLERK_API_URL
 export const JWT_KEY = process.env.CLERK_JWT_KEY || ''
-// export const SDK_METADATA = {
-//   name: PACKAGE_NAME,
-//   version: PACKAGE_VERSION,
-//   environment: process.env.NODE_ENV,
-// };
 
 export const { Cookies, Headers } = constants
