c778ac39b73415a2afe993a6593af2fc9864eecc
wobsoriano <sorianorobertc@gmail.com>
2024-09-20 19:21:08 -0700

    chore: Disable handshake flow by default

--- src/withClerkMiddleware.ts
+++ src/withClerkMiddleware.ts
@@ -6,7 +6,12 @@ import { clerkClient } from './clerkClient'
 import * as constants from './constants'
 import { handshakeWithoutRedirect } from './errors'
 
-export function withClerkMiddleware(options?: ClerkOptions) {
+export type H3ClerkOptions = ClerkOptions & {
+  enableHandshake?: boolean
+}
+
+export function withClerkMiddleware(options?: H3ClerkOptions) {
+  const enableHandshake = options?.enableHandshake ?? true
   return eventHandler(async (event) => {
     const clerkRequest = toWebRequest(event)
 
@@ -16,20 +21,22 @@ export function withClerkMiddleware(options?: ClerkOptions) {
       publishableKey: options?.publishableKey ?? constants.PUBLISHABLE_KEY,
     })
 
-    const locationHeader = requestState.headers.get(constants.Headers.Location)
-    if (locationHeader) {
-      // Trigger a handshake redirect
-      return new Response(null, { status: 307, headers: requestState.headers })
-    }
+    if (enableHandshake) {
+      const locationHeader = requestState.headers.get(constants.Headers.Location)
+      if (locationHeader) {
+        // Trigger a handshake redirect
+        return new Response(null, { status: 307, headers: requestState.headers })
+      }
 
-    if (requestState.status === AuthStatus.Handshake) {
-      throw new Error(handshakeWithoutRedirect)
-    }
+      if (requestState.status === AuthStatus.Handshake) {
+        throw new Error(handshakeWithoutRedirect)
+      }
 
-    if (requestState.headers) {
-      requestState.headers.forEach((value, key) => {
-        setResponseHeader(event, key, value)
-      })
+      if (requestState.headers) {
+        requestState.headers.forEach((value, key) => {
+          setResponseHeader(event, key, value)
+        })
+      }
     }
 
     event.context.auth = requestState.toAuth()
