d6990afd7e3836e4f07b34f22871a5dcc6cdf8f5
wobsoriano <sorianorobertc@gmail.com>
2024-12-10 14:46:41 -0800

    chore: remove handshake option, enabled by default

--- src/withClerkMiddleware.ts
+++ src/withClerkMiddleware.ts
@@ -6,22 +6,7 @@ import * as constants from './constants'
 import { handshakeWithoutRedirect } from './errors'
 import { toWebRequest } from './utils'
 
-export type H3ClerkOptions = ClerkOptions & {
-  /**
-   * Enables Clerk's handshake flow, which helps verify the session state
-   * when a session JWT has expired. It issues a 307 redirect to refresh
-   * the session JWT if the user is still logged in.
-   *
-   * This is useful for server-rendered fullstack applications to handle
-   * expired JWTs securely and maintain session continuity.
-   *
-   * @default true
-   */
-  enableHandshake?: boolean
-}
-
-export function withClerkMiddleware(options?: H3ClerkOptions) {
-  const enableHandshake = options?.enableHandshake ?? true
+export function withClerkMiddleware(options?: ClerkOptions) {
   return eventHandler(async (event) => {
     const clerkRequest = toWebRequest(event)
 
@@ -31,22 +16,20 @@ export function withClerkMiddleware(options?: H3ClerkOptions) {
       publishableKey: options?.publishableKey ?? constants.PUBLISHABLE_KEY,
     })
 
-    if (enableHandshake) {
-      const locationHeader = requestState.headers.get(constants.Headers.Location)
-      if (locationHeader) {
-        // Trigger a handshake redirect
-        return new Response(null, { status: 307, headers: requestState.headers })
-      }
+    const locationHeader = requestState.headers.get(constants.Headers.Location)
+    if (locationHeader) {
+      // Trigger a handshake redirect
+      return new Response(null, { status: 307, headers: requestState.headers })
+    }
 
-      if (requestState.status === AuthStatus.Handshake) {
-        throw new Error(handshakeWithoutRedirect)
-      }
+    if (requestState.status === AuthStatus.Handshake) {
+      throw new Error(handshakeWithoutRedirect)
+    }
 
-      if (requestState.headers) {
-        requestState.headers.forEach((value, key) => {
-          setResponseHeader(event, key, value)
-        })
-      }
+    if (requestState.headers) {
+      requestState.headers.forEach((value, key) => {
+        setResponseHeader(event, key, value)
+      })
     }
 
     event.context.auth = requestState.toAuth()
