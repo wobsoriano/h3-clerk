f9d354c909955b3b6899c15934f9831df0bdd354
wobsoriano <sorianorobertc@gmail.com>
2025-11-03 05:20:14 -0800

    chore: export ClerkMiddlewareOptions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { clerkClient } from './clerkClient'
-export { clerkMiddleware } from './clerkMiddleware'
+export { clerkMiddleware, type ClerkMiddlewareOptions } from './clerkMiddleware'
 
 export { getAuth } from './getAuth'
 export * from '@clerk/backend'
